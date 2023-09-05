a075ebc6c6d9246cffa6d29f49fc6559b834e453
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-09-05 07:26:18 +0000

    commit

--- ground.js
+++ ground.js
@@ -12,3 +12,13 @@ export function setupGround() {
     setCustomProperty(groundElems[1], "--left", 300)
   }
   
+  export function updateGround(delta, speedScale) {
+    groundElems.forEach(ground => {
+      incrementCustomProperty(ground, "--left", delta * speedScale * SPEED * -1)
+  
+      if (getCustomProperty(ground, "--left") <= -300) {
+        incrementCustomProperty(ground, "--left", 600)
+      }
+    })
+  }
+  
