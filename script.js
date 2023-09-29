7a590f28402b0caec4b1c2be0839f82430c489a9
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-09-29 09:33:34 +0000

    commit

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+import { updateGround, setupGround } from "./ground.js"
+import { updateDino, setupDino, getDinoRect, setDinoLose } from "./dino.js"
+import { updateCactus, setupCactus, getCactusRects } from "./cactus.js"
 
+const WO
