717739423cdb9517820d268a7941edfb01be17e2
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-10-23 08:54:53 +0000

    commit

--- script.js
+++ script.js
@@ -71,7 +71,10 @@ function  update(time){
     },100)
   }
 
-  function setPixelToWorldScale(){
-    let worldToPixelScale
-    if (window.innerWidth / window.innerHeight < WORLD_WIDTH/)
+ function setPixelToWorldScale() {
+  let worldToPixelScale
+  if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+    worldToPixelScale = window.innerWidth / WORLD_WIDTH
+  } else {
+    worldToPixelScale = window.innerHeight / WORLD_HEIGHT
   }
