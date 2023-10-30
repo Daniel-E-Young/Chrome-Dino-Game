d698b01a21a9c5c97cefc5826384ced769a9aeb0
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-10-30 09:30:47 +0000

    commt

--- script.js
+++ script.js
@@ -80,4 +80,4 @@ function  update(time){
   }
 
   worldElem.style.width = '${WORLD_WIDTH * worldToPixelScale}px'
-  worldElem.style.height = 
+  worldElem.style.height = '${WORLD_HEIGHT * worldToPixel'
