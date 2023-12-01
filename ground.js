5b3df9521410ec4bca5a778b8e543339600f83ec
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-12-01 07:57:08 +0000

    commit

--- ground.js
+++ ground.js
@@ -8,7 +8,7 @@ const SPEED = 0.05
 const groundElems = document.querySelectorAll("[data-round]")
 
 export function setupGround() {
-    setCustomProperty(groundElems[0], "--left", 1)
+    setCustomProperty(groundElems[0], "--left", 10)
     setCustomProperty(groundElems[1], "--left", 300)
   }
   
