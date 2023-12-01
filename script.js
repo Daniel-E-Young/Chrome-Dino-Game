caa2691d59103ed4dc7f83756ee03b2e18e5f02e
Daniel Young <109361146+Daniel-E-Young@users.noreply.github.com>
2023-12-01 07:55:32 +0000

    commi

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ function handleLose() {
   setTimeout(() => {
     document.addEventListener("keydown", handleStart, { once: true })
     startScreenElem.classList.remove("hide")
-  }, 100)
+  }, 1000)
 }
 
 function setPixelToWorldScale() {
